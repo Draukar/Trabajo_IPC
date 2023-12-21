e793027efd98581347ab28d761714915335f82b9
Pannik03 <alba.elep@gmail.com>
2023-12-21 11:19:09 +0100

    Ahora si, el main definitivo
    He cambiado el nombre de una carpeta, pero sigue estando el asunto de que esto tiene que ser tal cual el main

--- IPC_trabajo/src/inicio/Utils.java
+++ IPC_trabajo/src/inicio/Utils.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxmlapplication;
+package inicio;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
