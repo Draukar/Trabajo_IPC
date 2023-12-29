370e60b5d1a5fa83bba4d7f29f9323ad26a67cb2
Pannik03 <alba.elep@gmail.com>
2023-12-29 13:54:00 +0100

    Errores varios

--- IPC_trabajo/src/Controllers/ContactoController.java
+++ IPC_trabajo/src/Controllers/ContactoController.java
@@ -1,4 +1,32 @@
+<<<<<<< Updated upstream
 package Controllers;
 
 public class ContactoController {
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author elena
+ */
+public class ContactoController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+>>>>>>> Stashed changes
 }
