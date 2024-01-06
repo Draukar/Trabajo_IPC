796ddae8cbc43d01a8df5f1773427cb57bf434a7
sofiaamartinezz <soff.m.p.03@gmail.com>
2024-01-06 20:26:41 +0100

    Categoria
    Estoy programando el añadir categoria
    Cosas que faltan:
    como guardar el color junto a la categoria (ni idea de como hacerlo)
    el boton limpiar (luego lo hago)

--- IPC_trabajo/src/Controllers/GastoController.java
+++ IPC_trabajo/src/Controllers/GastoController.java
@@ -2,10 +2,13 @@ package Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 
 public class GastoController implements Initializable {
+
 
     /**
      * Initializes the controller class.
--- IPC_trabajo/src/Controllers/MenuController.java
+++ IPC_trabajo/src/Controllers/MenuController.java
@@ -6,6 +6,7 @@ import Model.Model;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.stage.Stage;
 
 public class MenuController implements Initializable {
     public Button boton_inicio;
@@ -17,6 +18,17 @@ public class MenuController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-    
+        boton_anadir.setOnAction(actionEvent -> anadir());
+        //boton_historial.setOnAction(actionEvent -> historial());
+        //boton_perfil.setOnAction(actionEvent -> registro());
+        boton_contacto.setOnAction(actionEvent -> Model.getInstance().getMainView().ventanaContacto());
     }
+    
+    public void anadir(){
+        Stage stage = (Stage) boton_anadir.getScene().getWindow();
+        Model.getInstance().getMainView().ventanaGasto();
+        stage.showAndWait();
+    }
+    
+    
 }
